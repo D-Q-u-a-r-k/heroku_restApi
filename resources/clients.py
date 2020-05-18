226caf98a6edf68870727f3f8455aedd51f8622d
DQuark <amanprakash180601@gmail.com>
2020-05-19 05:24:28 +0530

    client get update

--- resources/clients.py
+++ resources/clients.py
@@ -1,4 +1,18 @@
-from flask_restful import Resource
+from flask_restful import reqparse, abort, Api, Resource
+
+CLIENTS = {
+    'client1': {'task': 'build an API'},
+    'client2': {'task': '?????'},
+    'client3': {'task': 'profit!'},
+}
+
+def abort_if_client_doesnt_exist(todo_id):
+    if client_id not in CLIENTS:
+        abort(404, message="Todo {} doesn't exist".format(client_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
 
 class Clients(Resource):
     def get(self):
