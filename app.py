518f50ce8db77d034aef8f88dfd3c6f660a5b75d
DQuark <amanprakash180601@gmail.com>
2020-05-20 06:59:57 +0530

    json request working

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
 from flask_restful import Api
-from resources.clients import ClientsQuery, ClientsSubmit
-from resources.devs import DevsListAll
+from resources.clients import Clients
+from resources.devs import Devs
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route("/")
-def webPage():
+def landingPage():
     return render_template("index.html", stringPrint = "Hello World!!")
 
-api.add_resource(ClientsQuery, "/clients/<client_id>")
-api.add_resource(ClientsSubmit, "/clients")
-api.add_resource(DevsListAll, "/devs")
+api.add_resource(Clients, "/clients", "/clients/<client_id>")
+api.add_resource(Devs, "/devs")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- resources/clients.py
+++ resources/clients.py
@@ -17,22 +17,21 @@ parser.add_argument('deadline')
 parser.add_argument('services')
 parser.add_argument('description')
 
-class ClientsQuery(Resource):
+class Clients(Resource):
     def get(self, client_id):
         abort_if_client_doesnt_exist(client_id)
         return CLIENTS[client_id]
 
-class ClientsSubmit(Resource):
     def post(self):
-        args = parser.parse_args()
-        client_id = int(max(CLIENTS.keys()).lstrip('client')) + 1
-        client_id = 'client%i' % client_id
-        CLIENTS[client_id] = {'Full Name': args['full_name'],
-                                'Email': args['email'],
-                                'Phone': args['phone'],
-                                'Deadline': args['deadline'],
-                                'Services': args['services'],
-                                'Description': args['description']
-                                }
-        write(CLIENTS) #update json
-        return CLIENTS[client_id], 201
+            args = parser.parse_args()
+            client_id = int(max(CLIENTS.keys()).lstrip('client')) + 1
+            client_id = 'client%i' % client_id
+            CLIENTS[client_id] = {'Full Name': args['full_name'],
+                                    'Email': args['email'],
+                                    'Phone': args['phone'],
+                                    'Deadline': args['deadline'],
+                                    'Services': args['services'],
+                                    'Description': args['description']
+                                    }
+            write(CLIENTS) #update json
+            return CLIENTS[client_id], 201
